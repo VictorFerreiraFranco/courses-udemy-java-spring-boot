9dc3aee99f18ba39331b17f7a5f66d81d0b57789
Victor Franco <93090737+VictorFerreiraFranco@users.noreply.github.com>
2025-07-08 12:37:52 -0300

    courses: S3 - Ep 26

--- architecture/src/main/java/udemy/courses/architecture/architecture/automaker/api/FactoryController.java
+++ architecture/src/main/java/udemy/courses/architecture/architecture/automaker/api/FactoryController.java
@@ -3,10 +3,12 @@ package udemy.courses.architecture.architecture.automaker.api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import udemy.courses.architecture.architecture.automaker.*;
 
 @RestController
+@RequestMapping("/cars")
 public class FactoryController {
 
     @Autowired

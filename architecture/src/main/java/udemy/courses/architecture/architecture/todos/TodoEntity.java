64371d088f1810bd9c35c72ccc62636fcfe07dc4
Victor Franco <93090737+VictorFerreiraFranco@users.noreply.github.com>
2025-08-12 19:59:32 -0300

    courses: S3 - Ep 30

---
+++ architecture/src/main/java/udemy/courses/architecture/architecture/todos/TodoEntity.java
@@ -0,0 +1,43 @@
+package udemy.courses.architecture.architecture.todos;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_todo")
+public class TodoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    private String description;
+
+    @Column
+    private Boolean completed;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
+}
+

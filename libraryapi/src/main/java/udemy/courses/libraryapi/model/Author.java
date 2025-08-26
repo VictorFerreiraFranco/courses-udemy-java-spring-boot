f9ed3ad17e66cb4ac5abad146b07efd96f83feb6
Victor Franco <93090737+VictorFerreiraFranco@users.noreply.github.com>
2025-08-26 20:35:46 -0300

    courses: S4 - Ep 52

---
+++ libraryapi/src/main/java/udemy/courses/libraryapi/model/Author.java
@@ -0,0 +1,30 @@
+package udemy.courses.libraryapi.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "author", schema = "public")
+@Getter
+@Setter
+public class Author {
+
+    @Id
+    @Column()
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(length = 100, nullable = false)
+    private String name;
+
+    @Column(name = "date_birth", nullable = false)
+    private LocalDate birthDate;
+
+    @Column(length = 100, nullable = false)
+    private String nationality;
+}
+

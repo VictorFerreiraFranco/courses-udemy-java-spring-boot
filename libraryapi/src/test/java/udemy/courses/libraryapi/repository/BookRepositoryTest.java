b95d2374e242ae47a6a06b6eb1e41a6fdd0ea3e7
Victor Franco <93090737+VictorFerreiraFranco@users.noreply.github.com>
2025-08-30 09:38:27 -0300

    courses: S4 - Ep 62

--- libraryapi/src/test/java/udemy/courses/libraryapi/repository/BookRepositoryTest.java
+++ libraryapi/src/test/java/udemy/courses/libraryapi/repository/BookRepositoryTest.java
@@ -3,6 +3,7 @@ package udemy.courses.libraryapi.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 import udemy.courses.libraryapi.model.Author;
 import udemy.courses.libraryapi.model.Book;
 import udemy.courses.libraryapi.model.GenderBook;
@@ -64,16 +65,27 @@ class BookRepositoryTest {
     @Test
     void updateAuthorByBookTest()
     {
-        var book = bookRepository
+        Book book = bookRepository
                 .findById(UUID.fromString("dcd467ce-f2d0-4dbe-9965-c553eea2d7db"))
                 .orElse(null);
 
-        var author = authorRepository
+        Author author = authorRepository
                 .findById(UUID.fromString("e4523dda-4059-48ab-aaab-4c3fefc63191"))
                 .orElse(null);
 
         book.setAuthor(author);
 
         bookRepository.save(book);
+    }
+
+    @Test
+    @Transactional
+    void findBook(){
+        Book book = bookRepository
+                .findById(UUID.fromString("dcd467ce-f2d0-4dbe-9965-c553eea2d7db"))
+                .orElse(null);
+
+        System.out.println("Book:" + book.getTitle());
+        System.out.println("Author:" + book.getAuthor().getName());
     }
 }

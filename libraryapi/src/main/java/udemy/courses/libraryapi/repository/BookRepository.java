4eeb6a1b4f92b5bcb51ec83214e126db5dc19fa0
Victor Franco <93090737+VictorFerreiraFranco@users.noreply.github.com>
2025-08-30 16:36:43 -0300

    courses: S4 - Ep 69

--- libraryapi/src/main/java/udemy/courses/libraryapi/repository/BookRepository.java
+++ libraryapi/src/main/java/udemy/courses/libraryapi/repository/BookRepository.java
@@ -2,8 +2,10 @@ package udemy.courses.libraryapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import udemy.courses.libraryapi.model.Author;
 import udemy.courses.libraryapi.model.Book;
+import udemy.courses.libraryapi.model.GenderBook;
 
 import java.util.List;
 import java.util.UUID;
@@ -19,10 +21,10 @@ public interface BookRepository extends JpaRepository<Book, UUID> {
 
     List<Book> findByTitleContainingIgnoreCase(String title);
 
-    @Query(" select l from Book as l order by l.title ")
+    @Query("select l from Book as l order by l.title ")
     List<Book> findByAllPersonalized();
 
-    @Query(" select a from Book l join l.author a")
+    @Query("select a from Book l join l.author a")
     List<Author> findAuthorByBookPersonalized();
 
     @Query("""
@@ -33,4 +35,13 @@ public interface BookRepository extends JpaRepository<Book, UUID> {
         order by l.gender
     """)
     List<String> findAllGenderAuthorBrazilian();
+
+    @Query("select l from Book l where l.gender = :gender order by :parmOrder")
+    List<Book> findAllByGender(
+        @Param("gender") GenderBook genderBook,
+        @Param("parmOrder") String columnOrder
+    );
+
+    @Query("select l from Book l where l.gender = ?1 order by ?2")
+    List<Book> findAllByGenderPositionalParameter(GenderBook genderBook, String columnOrder);
 }

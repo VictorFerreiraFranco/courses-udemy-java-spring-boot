0284270408620be9190608bffe23c38985fd43b2
Victor <victor@gb-net.net>
2025-10-16 16:26:41 -0300

    courses: S8 - Ep 119

---
+++ libraryapi/src/main/java/udemy/courses/libraryapi/controller/LoginViewController.java
@@ -0,0 +1,13 @@
+package udemy.courses.libraryapi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginViewController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
